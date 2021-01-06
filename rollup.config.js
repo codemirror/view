79ff5c5d12061487859a2303d476492fe7a4fdf0
Marijn Haverbeke <marijn@haverbeke.nl>
2021-01-06 20:16:29 +0100

    Add CommonJS exports
    FEATURE: The package now also exports a CommonJS module.

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,14 @@ import dts from "rollup-plugin-dts"
 export default [{
   input: "./src/index.js",
   external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
-  output: {
+  output: [{
     format: "esm",
     file: "./dist/index.js",
     externalLiveBindings: false
-  }
+  }, {
+    format: "cjs",
+    file: "./dist/index.cjs"
+  }]
 }, {
   input: "./src/index.d.ts",
   output: {
