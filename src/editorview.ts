25cf472750370db3a3a561b9f073b004750414f0
Marijn Haverbeke <marijn@haverbeke.nl>
2021-05-17 17:04:26 +0200

    Remove data-gramm=false attribute
    Since the Grammarly browser plugin is apparently less invasive than it used
    to be.
    
    See https://discuss.codemirror.net/t/how-to-remove-default-attributes-from-content/3194

--- src/editorview.ts
+++ src/editorview.ts
@@ -347,9 +347,6 @@ export class EditorView {
       autocorrect: "off",
       autocapitalize: "off",
       contenteditable: String(this.state.facet(editable)),
-      // Disable the Grammarly extension, which otherwise feels free
-      // to randomly mess with our DOM structure.
-      "data-gramm": "false",
       class: "cm-content",
       style: `${browser.tabSize}: ${this.state.tabSize}`,
       role: "textbox",
