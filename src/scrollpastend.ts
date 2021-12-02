a31bb5fd65de13d6a92b1a5abf4a309123da7edb
Marijn Haverbeke <marijn@haverbeke.nl>
2021-12-02 14:19:08 +0100

    Simplify scrollPastEnd extension
    FIX: The `scrollPastEnd` extension will now over-reserve space at the bottom
    of the editor on startup, to prevent restored scroll positions from being clipped.

--- src/scrollpastend.ts
+++ src/scrollpastend.ts
@@ -1,36 +1,26 @@
 import {Extension} from "@codemirror/state"
-import {EditorView} from "./editorview"
-import {ViewPlugin, ViewUpdate, MeasureRequest} from "./extension"
+import {ViewPlugin, ViewUpdate, contentAttributes} from "./extension"
 
 const plugin = ViewPlugin.fromClass(class {
-  height = -1
-  measure: MeasureRequest<number>
-
-  constructor(view: EditorView) {
-    this.measure = {
-      read: view => Math.max(0, view.scrollDOM.clientHeight - view.defaultLineHeight),
-      write: (value, view) => {
-        if (Math.abs(value - this.height) > 1) {
-          this.height = value
-          view.contentDOM.style.paddingBottom = value + "px"
-        }
-      }
-    }
-    view.requestMeasure(this.measure)
-  }
+  height = 1000
+  attrs = {style: "padding-bottom: 1000px"}
 
   update(update: ViewUpdate) {
-    if (update.geometryChanged) update.view.requestMeasure(this.measure)
+    let height = update.view.viewState.editorHeight - update.view.defaultLineHeight
+    if (height != this.height) {
+      this.height = height
+      this.attrs = {style: `padding-bottom: ${height}px`}
+    }
   }
 })
 
-/// Returns a plugin that makes sure the content has a bottom margin
-/// equivalent to the height of the editor, minus one line height, so
-/// that every line in the document can be scrolled to the top of the
-/// editor.
+/// Returns an extension that makes sure the content has a bottom
+/// margin equivalent to the height of the editor, minus one line
+/// height, so that every line in the document can be scrolled to the
+/// top of the editor.
 ///
 /// This is only meaningful when the editor is scrollable, and should
 /// not be enabled in editors that take the size of their content.
 export function scrollPastEnd(): Extension {
-  return plugin
+  return [plugin, contentAttributes.of(view => view.plugin(plugin)?.attrs || null)]
 }
