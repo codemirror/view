322189754c1f6642c15c2a3a7b9d386a2d203a37
Marijn Haverbeke <marijn@haverbeke.nl>
2022-02-16 08:45:23 +0100

    Make sure DOMObserver.clear properly marks content views dirty
    FIX: Fix an issue where some keys on Android phones could leave
    the editor DOM unsynced with the actual document.
    
    See https://discuss.codemirror.net/t/command-insertnewlineandindent-causes-a-view-bug-unless-used-with-highlightactiveline-extension/3985

--- src/domobserver.ts
+++ src/domobserver.ts
@@ -218,7 +218,7 @@ export class DOMObserver {
 
   // Throw away any pending changes
   clear() {
-    this.observer.takeRecords()
+    this.processRecords()
     this.queue.length = 0
     this.selectionChanged = false
   }
