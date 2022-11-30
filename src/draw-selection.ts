494c5f304175ef1747b14a555caed12e37170331
Marijn Haverbeke <marijn@haverbeke.nl>
2022-11-30 13:26:23 +0100

    Export RectangleMarker.forRange
    FEATURE: The static `RectangleMarker.forRange` method exposes the logic used
    by the editor to draw rectangles covering a selection range.
    
    Issue https://github.com/codemirror/dev/issues/989

--- src/draw-selection.ts
+++ src/draw-selection.ts
@@ -1,17 +1,10 @@
-import {EditorSelection, SelectionRange, Extension, Facet, combineConfig, Prec, EditorState} from "@codemirror/state"
-import {BlockType} from "./decoration"
-import {BlockInfo} from "./heightmap"
+import {EditorSelection, Extension, Facet, combineConfig, Prec, EditorState} from "@codemirror/state"
 import {ViewUpdate, nativeSelectionHidden} from "./extension"
 import {EditorView} from "./editorview"
 import {layer, RectangleMarker} from "./layer"
-import {Direction} from "./bidi"
 import browser from "./browser"
 
 const CanHidePrimary = !browser.ios // FIXME test IE
-
-// Added to selection rectangles vertical extent to prevent rounding
-// errors from introducing gaps in the rendered content.
-const enum C { Epsilon = 0.01 }
 
 type SelectionConfig = {
   /// The length of a full cursor blink cycle, in milliseconds.
@@ -72,8 +65,9 @@ const cursorLayer = layer({
     for (let r of state.selection.ranges) {
       let prim = r == state.selection.main
       if (r.empty ? !prim || CanHidePrimary : conf.drawRangeCursor) {
-        let piece = measureCursor(view, r, prim)
-        if (piece) cursors.push(piece)
+        let className = prim ? "cm-cursor cm-cursor-primary" : "cm-cursor cm-cursor-secondary"
+        let cursor = r.empty ? r : EditorSelection.cursor(r.head, r.head > r.anchor ? -1 : 1)
+        for (let piece of RectangleMarker.forRange(view, className, cursor)) cursors.push(piece)
       }
     }
     return cursors
@@ -98,7 +92,8 @@ function setBlinkRate(state: EditorState, dom: HTMLElement) {
 const selectionLayer = layer({
   above: false,
   markers(view) {
-    return view.state.selection.ranges.map(r => r.empty ? [] : measureRange(view, r)).reduce((a, b) => a.concat(b))
+    return view.state.selection.ranges.map(r => r.empty ? [] : RectangleMarker.forRange(view, "cm-selectionBackground", r))
+      .reduce((a, b) => a.concat(b))
   },
   update(update, dom) {
     return update.docChanged || update.selectionSet || update.viewportChanged || configChanged(update)
@@ -114,120 +109,3 @@ const themeSpec = {
 }
 if (CanHidePrimary) (themeSpec as any)[".cm-line"].caretColor = "transparent !important"
 const hideNativeSelection = Prec.highest(EditorView.theme(themeSpec))
-
-function getBase(view: EditorView) {
-  let rect = view.scrollDOM.getBoundingClientRect()
-  let left = view.textDirection == Direction.LTR ? rect.left : rect.right - view.scrollDOM.clientWidth
-  return {left: left - view.scrollDOM.scrollLeft, top: rect.top - view.scrollDOM.scrollTop}
-}
-
-function wrappedLine(view: EditorView, pos: number, inside: {from: number, to: number}) {
-  let range = EditorSelection.cursor(pos)
-  return {from: Math.max(inside.from, view.moveToLineBoundary(range, false, true).from),
-          to: Math.min(inside.to, view.moveToLineBoundary(range, true, true).from),
-          type: BlockType.Text}
-}
-
-function blockAt(view: EditorView, pos: number): BlockInfo {
-  let line = view.lineBlockAt(pos)
-  if (Array.isArray(line.type)) for (let l of line.type) {
-    if (l.to > pos || l.to == pos && (l.to == line.to || l.type == BlockType.Text)) return l
-  }
-  return line as any
-}
-
-function measureRange(view: EditorView, range: SelectionRange): RectangleMarker[] {
-  if (range.to <= view.viewport.from || range.from >= view.viewport.to) return []
-  let from = Math.max(range.from, view.viewport.from), to = Math.min(range.to, view.viewport.to)
-
-  let ltr = view.textDirection == Direction.LTR
-  let content = view.contentDOM, contentRect = content.getBoundingClientRect(), base = getBase(view)
-  let lineStyle = window.getComputedStyle(content.firstChild as HTMLElement)
-  let leftSide = contentRect.left + parseInt(lineStyle.paddingLeft) + Math.min(0, parseInt(lineStyle.textIndent))
-  let rightSide = contentRect.right - parseInt(lineStyle.paddingRight)
-
-  let startBlock = blockAt(view, from), endBlock = blockAt(view, to)
-  let visualStart: {from: number, to: number} | null = startBlock.type == BlockType.Text ? startBlock : null
-  let visualEnd: {from: number, to: number} | null = endBlock.type == BlockType.Text ? endBlock : null
-  if (view.lineWrapping) {
-    if (visualStart) visualStart = wrappedLine(view, from, visualStart)
-    if (visualEnd) visualEnd = wrappedLine(view, to, visualEnd)
-  }
-  if (visualStart && visualEnd && visualStart.from == visualEnd.from) {
-    return pieces(drawForLine(range.from, range.to, visualStart))
-  } else {
-    let top = visualStart ? drawForLine(range.from, null, visualStart) : drawForWidget(startBlock, false)
-    let bottom = visualEnd ? drawForLine(null, range.to, visualEnd) : drawForWidget(endBlock, true)
-    let between = []
-    if ((visualStart || startBlock).to < (visualEnd || endBlock).from - 1)
-      between.push(piece(leftSide, top.bottom, rightSide, bottom.top))
-    else if (top.bottom < bottom.top && view.elementAtHeight((top.bottom + bottom.top) / 2).type == BlockType.Text)
-      top.bottom = bottom.top = (top.bottom + bottom.top) / 2
-    return pieces(top).concat(between).concat(pieces(bottom))
-  }
-
-  function piece(left: number, top: number, right: number, bottom: number) {
-    return new RectangleMarker("cm-selectionBackground",
-                                left - base.left, top - base.top - C.Epsilon, right - left, bottom - top + C.Epsilon)
-  }
-  function pieces({top, bottom, horizontal}: {top: number, bottom: number, horizontal: number[]}) {
-    let pieces = []
-    for (let i = 0; i < horizontal.length; i += 2)
-      pieces.push(piece(horizontal[i], top, horizontal[i + 1], bottom))
-    return pieces
-  }
-
-  // Gets passed from/to in line-local positions
-  function drawForLine(from: null | number, to: null | number, line: {from: number, to: number}) {
-    let top = 1e9, bottom = -1e9, horizontal: number[] = []
-    function addSpan(from: number, fromOpen: boolean, to: number, toOpen: boolean, dir: Direction) {
-      // Passing 2/-2 is a kludge to force the view to return
-      // coordinates on the proper side of block widgets, since
-      // normalizing the side there, though appropriate for most
-      // coordsAtPos queries, would break selection drawing.
-      let fromCoords = view.coordsAtPos(from, (from == line.to ? -2 : 2) as any)!
-      let toCoords = view.coordsAtPos(to, (to == line.from ? 2 : -2) as any)!
-      top = Math.min(fromCoords.top, toCoords.top, top)
-      bottom = Math.max(fromCoords.bottom, toCoords.bottom, bottom)
-      if (dir == Direction.LTR)
-        horizontal.push(ltr && fromOpen ? leftSide : fromCoords.left,
-                        ltr && toOpen ? rightSide : toCoords.right)
-      else
-        horizontal.push(!ltr && toOpen ? leftSide : toCoords.left,
-                        !ltr && fromOpen ? rightSide : fromCoords.right)
-    }
-
-    let start = from ?? line.from, end = to ?? line.to
-    // Split the range by visible range and document line
-    for (let r of view.visibleRanges) if (r.to > start && r.from < end) {
-      for (let pos = Math.max(r.from, start), endPos = Math.min(r.to, end);;) {
-        let docLine = view.state.doc.lineAt(pos)
-        for (let span of view.bidiSpans(docLine)) {
-          let spanFrom = span.from + docLine.from, spanTo = span.to + docLine.from
-          if (spanFrom >= endPos) break
-          if (spanTo > pos)
-            addSpan(Math.max(spanFrom, pos), from == null && spanFrom <= start,
-                    Math.min(spanTo, endPos), to == null && spanTo >= end, span.dir)
-        }
-        pos = docLine.to + 1
-        if (pos >= endPos) break
-      }
-    }
-    if (horizontal.length == 0) addSpan(start, from == null, end, to == null, view.textDirection)
- 
-    return {top, bottom, horizontal}
-  }
-
-  function drawForWidget(block: BlockInfo, top: boolean) {
-    let y = contentRect.top + (top ? block.top : block.bottom)
-    return {top: y, bottom: y, horizontal: []}
-  }
-}
-
-function measureCursor(view: EditorView, cursor: SelectionRange, primary: boolean): RectangleMarker | null {
-  let pos = view.coordsAtPos(cursor.head, cursor.assoc || 1)
-  if (!pos) return null
-  let base = getBase(view)
-  return new RectangleMarker(primary ? "cm-cursor cm-cursor-primary" : "cm-cursor cm-cursor-secondary",
-                              pos.left - base.left, pos.top - base.top, -1, pos.bottom - pos.top)
-}
--- src/layer.ts
+++ src/layer.ts
@@ -1,6 +1,9 @@
-import {Extension, Facet, EditorState} from "@codemirror/state"
+import {Extension, Facet, EditorState, EditorSelection, SelectionRange} from "@codemirror/state"
 import {ViewPlugin, ViewUpdate} from "./extension"
 import {EditorView} from "./editorview"
+import {Direction} from "./bidi"
+import {BlockType} from "./decoration"
+import {BlockInfo} from "./heightmap"
 
 /// Markers shown in a [layer](#view.layer) must conform to this
 /// interface. They are created in a measuring phase, and have to
@@ -23,10 +26,11 @@ export interface LayerMarker {
 /// Implementation of [`LayerMarker`](#view.LayerMarker) that creates
 /// a rectangle at a given set of coordinates.
 export class RectangleMarker implements LayerMarker {
-  /// Create a marker with the given class and dimensions.
+  /// Create a marker with the given class and dimensions. If `width`
+  /// is null, the DOM element will get no width style.
   constructor(private className: string,
               private left: number, private top: number,
-              private width: number, private height: number) {}
+              private width: number | null, private height: number) {}
 
   draw() {
     let elt = document.createElement("div")
@@ -44,13 +48,142 @@ export class RectangleMarker implements LayerMarker {
   private adjust(elt: HTMLElement) {
     elt.style.left = this.left + "px"
     elt.style.top = this.top + "px"
-    if (this.width >= 0) elt.style.width = this.width + "px"
+    if (this.width != null) elt.style.width = this.width + "px"
     elt.style.height = this.height + "px"
   }
 
   eq(p: RectangleMarker) {
     return this.left == p.left && this.top == p.top && this.width == p.width && this.height == p.height &&
       this.className == p.className
+  }
+
+  /// Create a set of rectangles for the given selection range,
+  /// assigning them theclass`className`. Will create a single
+  /// rectangle for empty ranges, and a set of selection-style
+  /// rectangles covering the range's content (in a bidi-aware
+  /// way) for non-empty ones.
+  static forRange(view: EditorView, className: string, range: SelectionRange): readonly RectangleMarker[] {
+    if (range.empty) {
+      let pos = view.coordsAtPos(range.head, range.assoc || 1)
+      if (!pos) return []
+      let base = getBase(view)
+      return [new RectangleMarker(className, pos.left - base.left, pos.top - base.top, null, pos.bottom - pos.top)]
+    } else {
+      return rectanglesForRange(view, className, range)
+    }
+  }
+}
+
+function getBase(view: EditorView) {
+  let rect = view.scrollDOM.getBoundingClientRect()
+  let left = view.textDirection == Direction.LTR ? rect.left : rect.right - view.scrollDOM.clientWidth
+  return {left: left - view.scrollDOM.scrollLeft, top: rect.top - view.scrollDOM.scrollTop}
+}
+
+function wrappedLine(view: EditorView, pos: number, inside: {from: number, to: number}) {
+  let range = EditorSelection.cursor(pos)
+  return {from: Math.max(inside.from, view.moveToLineBoundary(range, false, true).from),
+          to: Math.min(inside.to, view.moveToLineBoundary(range, true, true).from),
+          type: BlockType.Text}
+}
+
+function blockAt(view: EditorView, pos: number): BlockInfo {
+  let line = view.lineBlockAt(pos)
+  if (Array.isArray(line.type)) for (let l of line.type) {
+    if (l.to > pos || l.to == pos && (l.to == line.to || l.type == BlockType.Text)) return l
+  }
+  return line as any
+}
+
+// Added to range rectangle's vertical extent to prevent rounding
+// errors from introducing gaps in the rendered content.
+const enum C { Epsilon = 0.01 }
+
+function rectanglesForRange(view: EditorView, className: string, range: SelectionRange): RectangleMarker[] {
+  if (range.to <= view.viewport.from || range.from >= view.viewport.to) return []
+  let from = Math.max(range.from, view.viewport.from), to = Math.min(range.to, view.viewport.to)
+
+  let ltr = view.textDirection == Direction.LTR
+  let content = view.contentDOM, contentRect = content.getBoundingClientRect(), base = getBase(view)
+  let lineStyle = window.getComputedStyle(content.firstChild as HTMLElement)
+  let leftSide = contentRect.left + parseInt(lineStyle.paddingLeft) + Math.min(0, parseInt(lineStyle.textIndent))
+  let rightSide = contentRect.right - parseInt(lineStyle.paddingRight)
+
+  let startBlock = blockAt(view, from), endBlock = blockAt(view, to)
+  let visualStart: {from: number, to: number} | null = startBlock.type == BlockType.Text ? startBlock : null
+  let visualEnd: {from: number, to: number} | null = endBlock.type == BlockType.Text ? endBlock : null
+  if (view.lineWrapping) {
+    if (visualStart) visualStart = wrappedLine(view, from, visualStart)
+    if (visualEnd) visualEnd = wrappedLine(view, to, visualEnd)
+  }
+  if (visualStart && visualEnd && visualStart.from == visualEnd.from) {
+    return pieces(drawForLine(range.from, range.to, visualStart))
+  } else {
+    let top = visualStart ? drawForLine(range.from, null, visualStart) : drawForWidget(startBlock, false)
+    let bottom = visualEnd ? drawForLine(null, range.to, visualEnd) : drawForWidget(endBlock, true)
+    let between = []
+    if ((visualStart || startBlock).to < (visualEnd || endBlock).from - 1)
+      between.push(piece(leftSide, top.bottom, rightSide, bottom.top))
+    else if (top.bottom < bottom.top && view.elementAtHeight((top.bottom + bottom.top) / 2).type == BlockType.Text)
+      top.bottom = bottom.top = (top.bottom + bottom.top) / 2
+    return pieces(top).concat(between).concat(pieces(bottom))
+  }
+
+  function piece(left: number, top: number, right: number, bottom: number) {
+    return new RectangleMarker(className, left - base.left, top - base.top - C.Epsilon,
+                               right - left, bottom - top + C.Epsilon)
+  }
+  function pieces({top, bottom, horizontal}: {top: number, bottom: number, horizontal: number[]}) {
+    let pieces = []
+    for (let i = 0; i < horizontal.length; i += 2)
+      pieces.push(piece(horizontal[i], top, horizontal[i + 1], bottom))
+    return pieces
+  }
+
+  // Gets passed from/to in line-local positions
+  function drawForLine(from: null | number, to: null | number, line: {from: number, to: number}) {
+    let top = 1e9, bottom = -1e9, horizontal: number[] = []
+    function addSpan(from: number, fromOpen: boolean, to: number, toOpen: boolean, dir: Direction) {
+      // Passing 2/-2 is a kludge to force the view to return
+      // coordinates on the proper side of block widgets, since
+      // normalizing the side there, though appropriate for most
+      // coordsAtPos queries, would break selection drawing.
+      let fromCoords = view.coordsAtPos(from, (from == line.to ? -2 : 2) as any)!
+      let toCoords = view.coordsAtPos(to, (to == line.from ? 2 : -2) as any)!
+      top = Math.min(fromCoords.top, toCoords.top, top)
+      bottom = Math.max(fromCoords.bottom, toCoords.bottom, bottom)
+      if (dir == Direction.LTR)
+        horizontal.push(ltr && fromOpen ? leftSide : fromCoords.left,
+                        ltr && toOpen ? rightSide : toCoords.right)
+      else
+        horizontal.push(!ltr && toOpen ? leftSide : toCoords.left,
+                        !ltr && fromOpen ? rightSide : fromCoords.right)
+    }
+
+    let start = from ?? line.from, end = to ?? line.to
+    // Split the range by visible range and document line
+    for (let r of view.visibleRanges) if (r.to > start && r.from < end) {
+      for (let pos = Math.max(r.from, start), endPos = Math.min(r.to, end);;) {
+        let docLine = view.state.doc.lineAt(pos)
+        for (let span of view.bidiSpans(docLine)) {
+          let spanFrom = span.from + docLine.from, spanTo = span.to + docLine.from
+          if (spanFrom >= endPos) break
+          if (spanTo > pos)
+            addSpan(Math.max(spanFrom, pos), from == null && spanFrom <= start,
+                    Math.min(spanTo, endPos), to == null && spanTo >= end, span.dir)
+        }
+        pos = docLine.to + 1
+        if (pos >= endPos) break
+      }
+    }
+    if (horizontal.length == 0) addSpan(start, from == null, end, to == null, view.textDirection)
+
+    return {top, bottom, horizontal}
+  }
+
+  function drawForWidget(block: BlockInfo, top: boolean) {
+    let y = contentRect.top + (top ? block.top : block.bottom)
+    return {top: y, bottom: y, horizontal: []}
   }
 }
 
